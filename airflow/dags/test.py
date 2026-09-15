17556ba6bd68e377e390bb05053f8aaca655f8a6
Vlad <mun.vladislav.a@gmail.com>
2026-09-15 13:32:01 +0500

    airflow test

--- airflow/dags/test.py
+++ airflow/dags/test.py
@@ -1,6 +1,6 @@
-from airflow import DAG
-from airflow.providers.standard.operators.python import PythonOperator
 from datetime import datetime
+from airflow.sdk import DAG
+from airflow.providers.standard.operators.python import PythonOperator
 
 
 def airflow_test():
